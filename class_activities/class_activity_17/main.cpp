8916c9032121cc8acd0e218860185a4cca7662dd
trkk1 <trankiett0911@gmail.com>
2024-10-10 13:38:01 -0700

    linear search algortithm

---
+++ class_activities/class_activity_17/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  CIS25
+//
+//  Created by Minh Tran on 10/10/24.
+//
+
+#include <stdio.h>
+#include <iostream>
+#include "search_functions.hpp"
+using namespace std;
+using namespace search;
+
+
+int main() {
+    int listNumbers[] = { 2, 3, 4, 10, 40 };
+    int n = sizeof(listNumbers) / sizeof(listNumbers[0]);
+    int target = 10;
+    
+    int result = linearSearch(listNumbers, n, target);
+    
+    if (result != -1) {
+        cout << "Element found at index " << result << endl;
+    } else {
+        cout << "Element not found" << endl;
+    }
+    
+    return 0;
+}
