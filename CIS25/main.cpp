91c10d9098835c91b9414ff1d59bd34921268b01
trkk1 <trankiett0911@gmail.com>
2024-10-10 12:37:57 -0700

    Initial Commit

---
+++ CIS25/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CIS25
+//
+//  Created by Minh Tran on 10/10/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
